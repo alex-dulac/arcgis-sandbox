1c384e367b7b93f35c2c5ca938390013051af9fd
Alex Dulac <alex@alexdulac.dev>
2023-07-13 08:19:32 -0400

    ArcGIS Sandbox

---
+++ README.md
@@ -0,0 +1,16 @@
+# ArcGIS + Vue 3 + Vite
+
+Tinkering around with the ArcGIS Javascript SDK and Vue
+
+## Requirements
+
+- You'll need your own ArcGIS Developer API Key
+- Create a new file `.env` and populate your key in the parameter, see `.env.example`
+
+## ArcGIS Docs
+
+https://developers.arcgis.com/javascript/latest/
+
+## Run locally
+- `npm install`
+- `npm run dev`
---
+++ src/components/Map.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import config from '@arcgis/core/config.js';
+import Map from '@arcgis/core/Map.js';
+import MapView from '@arcgis/core/views/MapView.js';
+
+const mapDiv = ref();
+
+onMounted(() => {
+  config.apiKey = import.meta.env.VITE_API_KEY;
+
+  const map = new Map({
+    basemap: 'arcgis-nova'
+  });
+
+  new MapView({
+    map,
+    container: mapDiv.value,
+    center: [-70, 43],
+    zoom: 8
+  })
+})
+</script>
+
+<template>
+  <div class="map-container" ref="mapDiv"></div>
+</template>
+
+<style scoped>
+.map-container {
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+}
+</style>
